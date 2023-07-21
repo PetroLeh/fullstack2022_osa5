60a806b7f866ec6fe597691ac8a72a1fc839687e
Petro Lehtonen <petro.lehtonen@gmail.com>
2023-07-21 23:14:25 +0300

    teht 5.2

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
         const user = await loginService.login({
             username, password
         })
+        window.localStorage.setItem('loggedUser', JSON.stringify(user))
         setUser(user)
         setUsername('')
         setPassword('')
@@ -24,6 +25,14 @@ const App = () => {
         // error message
     }
   }
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+        const user = JSON.parse(loggedUserJSON)
+        setUser(user)
+    }
+  }, [])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -58,10 +67,17 @@ const App = () => {
     </div>
   )
 
+  const logout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedUser')
+  }
+
   const blogListing = () => (
     <div>
     <h2>blogs</h2>
-    <p>{ user.username } logged in</p>
+    <p>{ user.username } logged in
+    <button onClick={ logout }>logout</button>
+    </p>
     {blogs.map(blog =>
       <Blog key={blog.id} blog={blog} />
     )}
